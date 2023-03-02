0f7ad7db4c5411f3a01eaca7c34fe3645e011576
NiravChaudhari <nirav.16bca@gmail.com>
2023-03-02 16:04:52 +0530

    half work is done

--- passwordGenerator/script.js
+++ passwordGenerator/script.js
@@ -1,7 +1,17 @@
 console.log("password Generator");
 
-let password_length = document.querySelector("[data-passwordLength]");
-let inputSlider = document.querySelector("[data-inputSlider]");
+const password_length = document.querySelector("[data-passwordLength]");
+const inputSlider = document.querySelector("[data-inputSlider]");
+const checkboxs = document.querySelectorAll(".checkboxs");
+const indicator = document.querySelector(".strenth-color");
+
+//get checkbox 
+const checkUpperCase = document.getElementById("hasUpperCase");
+const checkLowerCase = document.getElementById("hasLowerCase");
+const checkNumber = document.getElementById("hasNumber");
+const checkSymboles = document.getElementById("hasSymboles");
+
+// Symbol String
 const symbole = "!@#$%^&*()_+[]{}<>?/~";
 
 // init
@@ -12,24 +22,23 @@ handleSlider()
 
 
 function handleSlider() {
-     inputSlider.value = passwordLength;
-     password_length.textContent = passwordLength;
+    inputSlider.value = passwordLength;
+    password_length.textContent = passwordLength;
 
-     let min = inputSlider.min;
-     let max = inputSlider.max;
-     
-     let a = inputSlider.style.backgroundSize = ((passwordLength - min) * 100 / (max - min)) + "% 100%";
-     console.log(passwordLength);
+    let min = inputSlider.min;
+    let max = inputSlider.max;
+
+    let a = inputSlider.style.backgroundSize = ((passwordLength - min) * 100 / (max - min)) + "% 100%";
 }
 // get value of input slider 
 inputSlider.addEventListener("input", (e) => {
-     passwordLength = e.target.value;
-     handleSlider()
+    passwordLength = e.target.value;
+    handleSlider()
 })
 
 // Random integer Generator function
-function getIntNumber(min,max) {
-     return Math.floor(Math.random() * (max - min) + min);
+function getIntNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 // Generate Random Number
 function generateRandomNumber() {
@@ -38,21 +47,40 @@ function generateRandomNumber() {
 
 // Generate Random UpperCase
 function generateRandomUpperCase() {
-     return String.fromCharCode(getIntNumber(65, 90));
+    return String.fromCharCode(getIntNumber(65, 90));
 }
 
 // Generate Random LowerCase
 function generateRandomLowerCase() {
-     return String.fromCharCode(getIntNumber(97, 122));
+    return String.fromCharCode(getIntNumber(97, 122));
 }
 
 // Generate Random Symbole
 function generateRandomSymbol() {
-     const randomIndexNum = getIntNumber(0, symbole.length)
-     return symbole.charAt(randomIndexNum);
+    const randomIndexNum = getIntNumber(0, symbole.length)
+    return symbole.charAt(randomIndexNum);
 }
 
+function setIndicator(color) {
+    indicator.style.backgroundColor = color;
+    indicator.style.boxShadow = `0px 0px 8px 1px ${color}`;
+}
 
+function caclStrength() {
 
+    let hasUpperCase = false;
+    let hasLowerCase = false;
+    let hasNumber = false;
+    let hasSymboles = false;
 
+    // check if checkedbox are checked or not
+    if (checkUpperCase.checked) hasUpperCase = true;
+    if (checkLowerCase.checked) hasLowerCase = true;
+    if (checkNumber.checked) hasNumber = true;
+    if (checkSymboles.checked) hasSymboles = true;
 
+    if (hasUpperCase && hasLowerCase && (hasNumber || hasSymboles) && passwordLength >= 8) {
+        setIndicator("")
+    }
+
+}
